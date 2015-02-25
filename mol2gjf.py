d1308b855def111cb31ad5d4d1be4bffeeb2c705
Chanin Nantasenamat <chaninn@users.noreply.github.com>
2015-02-25 10:42:16 +0700

    Create mol2gjf.py

---
+++ mol2gjf.py
@@ -0,0 +1,23 @@
+import os
+import glob
+import openbabel
+from pybel import *
+
+path = os.getcwd()+'/'
+out = glob.glob(path+'*.mol')
+
+outlist = []
+
+for i in out:
+    out_name_ext = i.split('/')
+    out_file_name, out_file_extension = out_name_ext[-1].split('.')
+    outlist.append(out_file_name)
+
+for i in range(len(outlist)):
+        conv = openbabel.OBConversion()
+        conv.SetInAndOutFormats("mol", "gjf")
+        conv.SetOptions("b", conv.OUTOPTIONS)
+
+        mol = openbabel.OBMol()
+        conv.ReadFile(mol, path + "/" + outlist[i] + ".mol")
+        conv.WriteFile(mol, path + "/" + outlist[i] + ".gjf")
